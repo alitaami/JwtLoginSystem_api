dfe9c56ab0f86ad3be4b977cbb236abb3c0e5fa2
Ali Taami <alitaami81@gmail.com>
2023-09-25 22:06:00 +0330

    Added Services

---
+++ Services/Services/Interfaces/IAccountService.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Base;
+using Entities.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services.Interfaces
+{
+    public interface IAccountService
+    {
+        public Task<ServiceResult> Login(TokenRequest tokenRequest, CancellationToken cancellationToken);
+        public Task<ServiceResult> UserSignUp(UserViewModel user, CancellationToken cancellationToken);
+
+    }
+}
